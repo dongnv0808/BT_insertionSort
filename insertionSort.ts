02ee00f50be6d7b89953220f8b6205f265e9b800
dongnv0808 <yamaraja0808@gmail.com>
2022-06-17 15:46:16 +0700

    commit

---
+++ insertionSort.ts
@@ -0,0 +1,21 @@
+class InsertionSort{
+    static numbers: number[] = [5, 2, 3, 1, -4, 6, 6, 8, 8, 10, 9];
+    static insertionSort(numbers: number[]): void{
+        for(let i = 1; i < numbers.length; i++){
+            let currentNumber = numbers[i]
+            for(let j = i - 1; j >= 0; j--){
+                if(numbers[j] > currentNumber){
+                    numbers[j + 1] = numbers[j];
+                    numbers[j] = currentNumber;
+                }
+            }
+            console.log("numbers after the  " + i + "' sort: ");
+            for (let k = 0; k < numbers.length; k++) {
+                console.log(numbers[k] + "\t");
+            }
+            console.log();
+        }
+    }
+}
+console.log("\nBegin sort processing...");
+InsertionSort.insertionSort(InsertionSort.numbers);
